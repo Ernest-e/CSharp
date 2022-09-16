8865295eb828a400affa93fbf0740a0abe832643
Ernest <rabinovichernest@gmail.com>
2022-09-17 00:26:31 +0300

    Сделали задание 2 (найти максимум 3 чисел)

---
+++ homework01/task2/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Введите число a");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число c");
+int c =  Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (a > b)
+{
+    if (a > c)
+    {
+        max = a;
+    }
+    else
+    {
+        max = c;
+    }
+}
+else
+{
+    if (b > c)
+    {
+        max = b;
+    }
+    else
+    {
+        max = c;
+    }
+}
+Console.WriteLine($"Максимальное число: {max}");
