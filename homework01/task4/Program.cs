fd80eecb07e0238e649e853d6a6fbb3a95ff53c8
Ernest <rabinovichernest@gmail.com>
2022-09-17 00:57:00 +0300

    Выполнили задание 2 (нахождение четных чисел)

---
+++ homework01/task4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+int n = N / 2;
+int i = 1;
+
+Console.WriteLine($"Четные числа от 1 до числа {N}:");
+while (i <= n)
+{
+    int x = 2 * i;
+    i += 1;
+    Console.WriteLine(x);
+}
