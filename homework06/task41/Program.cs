774e36ffafcb15b40061845d409e103f4711fd29
Ernest <rabinovichernest@gmail.com>
2022-10-05 20:55:11 +0300

    Выполнили дз 6

---
+++ homework06/task41/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine ("Введите числа через пробел");
+
+int [] myArray = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+int CountOfDigitMoreThanNull (int [] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i ++)
+    {
+        if (array[i] > 0) count += 1;
+    }
+    return count;
+}
+
+void PrintArray (int [] array)
+{
+    for (int i = 0; i < array.Length; i ++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+
+int count = CountOfDigitMoreThanNull (myArray);
+Console.WriteLine("Чисел больше ноля " + count);
---
+++ homework06/task43/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+
+
+Console.WriteLine("прямые заданы уравнением y = kx+b");
+Console.WriteLine("Введите k1");
+double k1 = Double.Parse(Console.ReadLine());
+Console.WriteLine("Введите b1");
+double b1 = Double.Parse(Console.ReadLine());
+Console.WriteLine("Введите k2");
+double k2 = Double.Parse(Console.ReadLine());
+Console.WriteLine("Введите b2");
+double b2 = Double.Parse(Console.ReadLine());
+
+double x = (b2-b1)/(k1-k2);
+double y = k1*x + b1;
+
+Console.WriteLine($"точка пересечения прямых ({x};{y})");
+
+
+
